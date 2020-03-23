1fe32d291c4e6450b43aeeda0cc30b914f4d56f0
OskariAuvinen <55886851+OskariAuvinen@users.noreply.github.com>
2020-03-23 13:59:56 +0200

    backpohja luotu

---
+++ sql/createTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE loppuprojekti (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name varchar(255) NOT NULL,
+    restaurant varchar NOT NULL,
+    time integer NOT NULL
+);
---
+++ sql/database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE loppuprojekti;
