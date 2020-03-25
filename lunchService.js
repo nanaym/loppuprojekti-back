d833e93b754517ebfed64aeea9747af9d875603c
Hannamaa <hanna.m.valli@gmail.com>
2020-03-25 17:03:26 +0200

    korjattu lunchService.js tiedostoon client

--- lunchService.js
+++ lunchService.js
@@ -1,21 +1,14 @@
-const Pool = require('pg').Pool;
-require('dotenv').config();
-// Get Postgres entry information from dot dot dot dot .. rebooot : dot env
-const USER = process.env.PGUSER;
-const PASSWORD = process.env.PGPASSWORD;
+const {Client} = require('pg');
 
-const conopts = {
-    user: USER,
-    password: PASSWORD,
-    host: 'ec2-54-247-79-178.eu-west-1.compute.amazonaws.com',
-    database: 'd62gu0m1thikgc',
-    port: 5432
-};
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
 
-const pool = new Pool(conopts);
+client.connect();
 
 const getPeople = (cb) => {
-    pool.query('SELECT * from lounas ORDER BY name ASC', (err, results) => {
+    client.query('SELECT * from lounas ORDER BY name ASC', (err, results) => {
         if (err) throw err;
         console.dir(results);
         cb(results.rows);
@@ -23,7 +16,7 @@ const getPeople = (cb) => {
 }
 
 const getRestaurants = (cb) => {
-    pool.query('SELECT * from lounas ORDER BY restaurant, time ', (err, results) => {
+    client.query('SELECT * from lounas ORDER BY restaurant, time ', (err, results) => {
         if (err) throw err;
         console.dir(results);
         cb(results.rows);
@@ -31,7 +24,7 @@ const getRestaurants = (cb) => {
 }
 
 const getPerson = (id, cb) => {
-    pool.query('SELECT * FROM lounas WHERE id=$1', [id], (err, results) => {
+    client.query('SELECT * FROM lounas WHERE id=$1', [id], (err, results) => {
         if (err) throw err;
         console.dir(results.rows);
         cb(results.rows);
@@ -40,7 +33,7 @@ const getPerson = (id, cb) => {
 
 const insertPost = (newperson, cb) => {
     const { name, restaurant, time } = newperson;
-    pool.query('INSERT INTO lounas (name, restaurant, time) VALUES ($1, $2, $3)', [name, restaurant, time], (err, results) => {
+    client.query('INSERT INTO lounas (name, restaurant, time) VALUES ($1, $2, $3)', [name, restaurant, time], (err, results) => {
         if (err) throw err;
         console.dir(results);
         cb(results.rowCount);
@@ -48,7 +41,7 @@ const insertPost = (newperson, cb) => {
 }
 
 const deletePost = (id, cb) => {
-    pool.query('DELETE FROM lounas WHERE id=$1', [id], (err, results) => {
+    client.query('DELETE FROM lounas WHERE id=$1', [id], (err, results) => {
         if (err) throw err;
         console.dir(results);
         cb(results.rowCount);
@@ -57,7 +50,7 @@ const deletePost = (id, cb) => {
 
 const updatePost = (post, id, cb) => {
     const { name, restaurant, time } = post;
-    pool.query('UPDATE lounas SET name=$1, restaurant=$2, time=$3 WHERE id=$4', [name, restaurant, time, id], (err, results) => {
+    client.query('UPDATE lounas SET name=$1, restaurant=$2, time=$3 WHERE id=$4', [name, restaurant, time, id], (err, results) => {
         if (err) throw err;
         console.dir(results);
         cb(results.rowCount);
